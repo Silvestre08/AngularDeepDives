32a7d50d4f923cfa5cb32fa537f22aeb4fbc526a
Celso <celsosilvestre08@gmail.com>
2024-10-28 20:55:42 -0400

    Explain control value accessors

--- ReactiveForms/src/app/edit-contact/edit-contact.component.ts
+++ ReactiveForms/src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private contactsService: ContactsService) { }
+  firstName = new FormControl('Jim');
+  lastName = new FormControl();
+  dateOfBirth = new FormControl();
+  favoritesRank = new FormControl();
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactsService.getContact(contactId).subscribe(
+      (contact) => {
+        if(!contact) return;        
+        this.firstName.setValue(contact.firstName)
+        this.lastName.setValue(contact.lastName)
+        this.dateOfBirth.setValue(contact.dateOfBirth)
+        this.favoritesRank.setValue(contact.favoritesRanking)
+      }
+    )
   }
 
   saveContact() {
+    console.log(this.firstName.value);
 
   }
 }
