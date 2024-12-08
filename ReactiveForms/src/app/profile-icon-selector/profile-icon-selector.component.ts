36e3b846c295b841c020f297080ad4ee769ec3bb
Celso <celsosilvestre08@gmail.com>
2024-12-08 15:49:25 -0500

    add control value accessor

--- ReactiveForms/src/app/profile-icon-selector/profile-icon-selector.component.ts
+++ ReactiveForms/src/app/profile-icon-selector/profile-icon-selector.component.ts
@@ -1,43 +1,46 @@
-import { Component } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, forwardRef, Provider } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { profileIconNames } from './profile-icon-names';
+
+const PROFILE_ICON_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => ProfileIconSelectorComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'con-profile-icon-selector',
   templateUrl: './profile-icon-selector.component.html',
-  styleUrls: ['./profile-icon-selector.component.css']
+  styleUrls: ['./profile-icon-selector.component.css'],
+  providers: [PROFILE_ICON_VALUE_ACCESSOR],
 })
 export class ProfileIconSelectorComponent implements ControlValueAccessor {
-
   profileIcons = profileIconNames;
-  showAllIcons: boolean = true
+  showAllIcons: boolean = true;
   selectedIcon!: string | null;
-  
+
   onChange!: Function;
   onTouched!: Function;
 
-  iconSelected(icon: string){
+  iconSelected(icon: string) {
     this.showAllIcons = false;
-    console.log(icon)
+    console.log(icon);
     this.selectedIcon = icon;
-    this.onChange(icon)
+    this.onChange(icon);
   }
-
-  writeValue(icon: string | null): void{
-this.selectedIcon = icon;
-
-if(icon && icon !=='')
-  this.showAllIcons = false
-else
-this.showAllIcons = true;
-}
-registerOnChange(fn: any): void {
-  this.onChange =(icon: string) =>{fn(icon)}
-  // it is this callback that actually updates the data model.
-}
-registerOnTouched(fn: any): void {
-  this.onTouched = fn
-}
-setDisabledState?(isDisabled: boolean): void {
-}
+  writeValue(icon: string | null): void {
+    this.selectedIcon = icon;
+    if (icon && icon !== '') this.showAllIcons = false;
+    else this.showAllIcons = true;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = (icon: string) => {
+      fn(icon);
+    };
+    // it is this callback that actually updates the data model.
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {}
 }
